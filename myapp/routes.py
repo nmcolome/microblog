8bf24f103bdb2031bbc16f71f003c49582f6f0da
Natalia Colome <nmcolome@gmail.com>
2021-08-12 10:58:13 -0600

    Add base template

--- myapp/routes.py
+++ myapp/routes.py
@@ -1,7 +1,9 @@
 # Define the logic of the application
 from myapp import app
+from flask import render_template
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return 'Hello World!'
+    user = {'username': 'natalia'}
+    return render_template('index.html', title='Home', user=user)
