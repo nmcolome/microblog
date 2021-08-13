d144e4a222d1d11380a53fee629e8bd1a2cc9c01
Natalia Colome <nmcolome@gmail.com>
2021-08-13 15:14:52 -0600

    Add flask_login & UserMixin in model

--- myapp/models.py
+++ myapp/models.py
@@ -1,8 +1,9 @@
 from datetime import datetime
 from werkzeug.security import generate_password_hash, check_password_hash
-from myapp import db
+from flask_login import UserMixin
+from myapp import db, login
 
-class User(db.Model):
+class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), index=True, unique=True)
     email = db.Column(db.String(120), index=True, unique=True)
@@ -17,6 +18,12 @@ class User(db.Model):
 
     def check_password(self, password):
         return check_password_hash(self.password_hash, password)
+
+# flask-login keeps track of the logged in user by its id.
+# it knows nothing about the db, it needs the app's help to load a user
+@login.user_loader
+def load_user(id):
+    return User.query.get(int(id))
 
 
 class Post(db.Model):
