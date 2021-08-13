0d0a694932425a69dbaa2e92f2aec77c849ded82
Natalia Colome <nmcolome@gmail.com>
2021-08-12 22:57:12 -0600

    Setup shell context

--- microblog.py
+++ microblog.py
@@ -1,4 +1,10 @@
 #Top-level script that represents the application
 
 #This is where Flask obtains our app instance
-from myapp import app
+from myapp import app, db
+from myapp.models import User, Post
+
+# When I run flask shell my db & models are there already
+@app.shell_context_processor
+def make_shell_context():
+    return {'db': db, 'User': User, 'Post': Post}
