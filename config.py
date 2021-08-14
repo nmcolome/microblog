34e515c8b605939f6500689e5b50fe7d19939f59
Natalia Colome <nmcolome@gmail.com>
2021-08-14 15:54:59 -0600

    Add pagination and pagination navigation for posts

--- config.py
+++ config.py
@@ -12,3 +12,4 @@ class Config(object):
     MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
     MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
     ADMINS = [os.environ.get('ADMIN_EMAIL')]
+    POSTS_PER_PAGE = 25
