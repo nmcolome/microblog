3b53915b6f1af2211255dc65ac39ae2b83638283
Natalia Colome <nmcolome@gmail.com>
2021-08-13 23:19:03 -0600

    Add helper methods to User for followers & followers posts

--- myapp/models.py
+++ myapp/models.py
@@ -41,6 +41,23 @@ class User(UserMixin, db.Model):
         digest = md5(self.email.lower().encode('utf-8')).hexdigest()
         return f'https://www.gravatar.com/avatar/{digest}?d=identicon&s={size}'
 
+    def follow(self, user):
+        if not self.is_following(user):
+            self.followed.append(user)
+
+    def unfollow(self, user):
+        if self.is_following(user):
+            self.followed.remove(user)
+
+    def is_following(self, user):
+        return self.followed.filter(followers.c.followed_id == user.id).count() > 0
+
+    def followed_posts(self):
+        followed = Post.query.join(
+            followers, (followers.c.followed_id == Post.user_id)).filter(
+                followers.c.follower_id == self.id)
+        return followed.union(self.posts).order_by(Post.timestamp.desc())
+
 # flask-login keeps track of the logged in user by its id.
 # it knows nothing about the db, it needs the app's help to load a user
 @login.user_loader
