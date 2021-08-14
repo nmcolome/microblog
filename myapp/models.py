d1128a96addfd8899391365d69b01c22b6d6b5b1
Natalia Colome <nmcolome@gmail.com>
2021-08-13 20:00:00 -0600

    Add avatar through gravatar.com

--- myapp/models.py
+++ myapp/models.py
@@ -1,4 +1,5 @@
 from datetime import datetime
+from hashlib import md5
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import UserMixin
 from myapp import db, login
@@ -18,6 +19,10 @@ class User(UserMixin, db.Model):
 
     def check_password(self, password):
         return check_password_hash(self.password_hash, password)
+
+    def avatar(self, size):
+        digest = md5(self.email.lower().encode('utf-8')).hexdigest()
+        return f'https://www.gravatar.com/avatar/{digest}?d=identicon&s={size}'
 
 # flask-login keeps track of the logged in user by its id.
 # it knows nothing about the db, it needs the app's help to load a user
