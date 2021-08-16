cb0b799585db8bf819993063573c9bbb62d89d01
Natalia Colome <nmcolome@gmail.com>
2021-08-15 20:33:05 -0600

    Change references to use create_app()

--- microblog.py
+++ microblog.py
@@ -1,9 +1,11 @@
 #Top-level script that represents the application
 
 #This is where Flask obtains our app instance
-from myapp import app, db, cli
+from myapp import create_app, db, cli
 from myapp.models import User, Post
 
+app = create_app()
+cli.register(app)
 # When I run flask shell my db & models are there already
 @app.shell_context_processor
 def make_shell_context():
